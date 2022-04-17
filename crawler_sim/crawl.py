1c2a11d26a4100396dc0d10446f90f4ae7a96158
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-04-18 00:34:39 +0900

    ..???

--- crawler_sim/crawl.py
+++ crawler_sim/crawl.py
@@ -1,6 +1,10 @@
 import os
-import requests
-import json
+import sys
+import urllib.request
+from dotenv import dotenv_values
+config = dotenv_values("../.env")
+client_id = config["ID"]
+client_secret = config["PW"]
 def get_district():
     district = {}
     with open("../place.txt","rt",encoding= 'utf-8') as f :
@@ -10,10 +14,15 @@ def get_district():
                 break
             elif line == '':
                 break
+            encText = urllib.parse.quote(line)
+            url = "https://openapi.naver.com/v1/search/blog?query=" + encText
+            request = urllib.request.Request(url)
+            request.add_header("X-Naver-Client-Id",client_id)
+            request.add_header("X-Naver-Client-Secret",client_secret)
+            response = urllib.request.urlopen(request)
+            rescode = response.getcode()
+            if(rescode==200):
+                response_body = response.read()
+                print(response_body.decode('utf-8'))
             else:
-                tmp = line.split(',')
-                if tmp[0] not in district.keys():
-                    district[tmp[0]] = [tmp[1].replace('\n','')]
-                else:
-                    district[tmp[0]].append(tmp[1].replace('\n',''))
-                
+                print("Error Code:" + rescode)
