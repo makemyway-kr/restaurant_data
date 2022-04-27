0dbccbd5b506d7b34a56e90bfc873c2cc2f5259f
sangcheol12 <53574961+sangcheol12@users.noreply.github.com>
2022-04-28 00:37:43 +0900

    add address, callNum
    add address, callNum

--- crawling.py
+++ crawling.py
@@ -7,8 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-'''
-엑셀에서 데이터 추출(형식 : ㅇㅇ동 ㅁㅁ식당)
+#엑셀에서 데이터 추출(형식 : ㅇㅇ동 ㅁㅁ식당)
 filename = '관악구레스토랑.xlsx'
 restaurant_file = openpyxl.load_workbook(filename)
 restaurants = restaurant_file.worksheets[0]
@@ -19,20 +18,20 @@ for restaurant in restaurants.iter_rows(min_row=3):
     if address is not None:
         data.append([
             restaurant[16].value.split('(')[1].split(')')[0],
-            restaurant[18].value
+            restaurant[18].value,
+            restaurant[16].value
         ])
 
 print(data[0])
-'''
 
-data = [['봉천동','삼우식당'],['신림동','전주식당'],["숭실대","마루스시"]]
+#data = [['봉천동','삼우식당','주소1'],['중랑구','커피나무','주소2'],["숭실대","마루스시",'주소3']]
 
 json_data = {}
 file_path = "./restaurants.json"
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome('크롬드라이버경로',options=options)
+driver = webdriver.Chrome('./100./chromedriver',options=options)
 
 '''
 json에서 값 가져오는 코드
@@ -57,35 +56,64 @@ for i in data:
         newlink = driver.find_element(by='xpath',value = '//*[@id="info.search.place.list"]/li[1]/div[5]/div[4]/a[1]').send_keys(Keys.ENTER)
         driver.switch_to.window(driver.window_handles[-1])
 
-        rateNum = driver.find_element(by='xpath',value = '//*[@id="mArticle"]/div[1]/div[1]/div[2]/div/div/a[1]/span[1]').text
+        try:
+            rateNum = driver.find_element(by='xpath',value = '//*[@id="mArticle"]/div[1]/div[1]/div[2]/div/div/a[1]/span[1]').text
+        except Exception as e1:
+            rateNum = "평점 없음"
+            pass
 
-        Timelist = driver.find_element(by=By.CLASS_NAME, value = 'list_operation')
-        periodTime = Timelist.find_elements(by=By.CLASS_NAME, value = 'txt_operation')
-        detailTime = Timelist.find_elements(by=By.CLASS_NAME, value = 'time_operation')
+        try:
+            callNum = driver.find_element(by='xpath',value = '//*[@id="mArticle"]/div[1]/div[2]/div[3]/div/div[1]/span/span[1]').text
+        except Exception as e1:
+            callNum = "전화번호 없음"
+            pass
+
+        time = ' '
+        try:
+            Timelist = driver.find_element(by=By.CLASS_NAME, value = 'list_operation')
+            periodTime = Timelist.find_elements(by=By.CLASS_NAME, value = 'txt_operation')
+            detailTime = Timelist.find_elements(by=By.CLASS_NAME, value = 'time_operation')
+        except Exception as e1:
+            time = '시간 정보 없음'
 
         menus_dic = {}
-        menulist = driver.find_element(by=By.CLASS_NAME, value = 'list_menu')
-        menus = menulist.find_elements(by=By.CLASS_NAME, value = 'loss_word')
-        menus_price = menulist.find_elements(by=By.CLASS_NAME, value = 'price_menu')
-        for a,b in zip(menus,menus_price):
-            menus_dic[a.text] = b.text
+        try:
+            menulist = driver.find_element(by=By.CLASS_NAME, value = 'list_menu')
+            menus = menulist.find_elements(by=By.CLASS_NAME, value = 'loss_word')
+            menus_price = menulist.find_elements(by=By.CLASS_NAME, value = 'price_menu')
+            for a,b in zip(menus,menus_price):
+                menus_dic[a.text] = b.text
+        except Exception as e1:
+            pass
+
 
         print("평점 " + rateNum)
         content["평점"] = rateNum
 
-        if detailTime:
-            time = []
-            for i in periodTime:
-                time.append(i.text)
-                print(i.text)
-            content["영업 시간"] = time
-        else:
-            print("시간 정보 없음")
-            content["영업 시간"] = "시간 정보 없음"
+        print("전화번호 " + callNum)
+        content["전화번호"] = callNum
 
-        for i in menus_dic:
-            print("key: {}, value: {}".format(i, menus_dic[i]))
-        content["메뉴"] = menus_dic
+        print("주소 " + i[2])
+        content["주소"] = i[2]
+        if time == '시간 정보 없음':
+            print("시간 정보 없음")
+        else:
+            if detailTime:
+                time = []
+                for i in periodTime:
+                    time.append(i.text)
+                    print(i.text)
+                content["영업 시간"] = time
+            else:
+                print("시간 정보 없음")
+                content["영업 시간"] = "시간 정보 없음"
+
+        if not menus_dic:
+            print("메뉴 정보 없음")
+        else:
+            for i in menus_dic:
+                print("key: {}, value: {}".format(i, menus_dic[i]))
+                content["메뉴"] = menus_dic
 
         json_data[keyword] = list(content.items())
         driver.close()
