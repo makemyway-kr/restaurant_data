355d8f78242d19d6396c45f6dc6ec973d75e52f8
SamSim <jun78111209@gmail.com>
2022-05-11 23:17:55 +0900

    어노테이션 프로그램 완료

--- Data_classifier/Menu_charac/sorter_user_input.py
+++ Data_classifier/Menu_charac/sorter_user_input.py
@@ -1,8 +1,8 @@
 import json
+from operator import indexOf
 def ui():
     filename = input("데이터를 끌어올 파일명을 입력하세요: ")
     data = []
-    metadata = [0,0]
     try:
         with open(filename,'r',encoding = 'utf-8') as f:
             data = json.load(f)
@@ -10,19 +10,17 @@ def ui():
     except Exception as ex:
         filename = input("오류발생 . 데이터를 끌어올 파일명을 입력하세요 : ")
     to_erase = []
-    temperary_save = False
-    for i in data:
-        selection = input("해당 키워드를 삭제할까요? (Y/N) , 입력없음은 Y처리. C 는 중단하고 임시저장. "+i + ':')
-        if selection == 'N':
-            to_erase.append(i)
-        elif selection == "C":
-            temperary_save = True
+    start = indexOf(data[1],data[0])
+    for i in range(start,len(data[1])):
+        selection = input("해당 키워드를 삭제할까요? (y/n) , 입력없음은 n처리. c 는 중단하고 임시저장. "+data[1][i] + ':')
+        if selection == 'y':
+            data[0] = data[1][i]
+        elif selection == "c":
             break
+        else:
+            to_erase.append(data[1][i])
     savefile = input('결과를 저장할 파일명을 입력하세요: ')
-    if temperary_save == True:
-        with open (savefile,'a',encoding='utf-8') as f:
-            f.write(json.dumps())
-    data = [ i for i in data if i not in to_erase ]
+    data = [ data[1][i] for i in range (len(data[1])) if data[1][i] not in to_erase ]
     savefile = input('결과를 저장할 파일명을 입력하세요: ')
     with open (savefile,'w',encoding='utf-8') as f:
         f.write(json.dumps(data,ensure_ascii=False,indent=4))
